6f5ccad17c55c5cc9ba3778c236ebc704e9fb5ea
rodneybarreto <rwbarreto@gmail.com>
2025-11-26 18:31:49 -0300

    Implementa testes de integração

---
+++ src/it/java/br/com/rodneybarreto/mybatch/ReaderIT.java
@@ -0,0 +1,34 @@
+package br.com.rodneybarreto.mybatch;
+
+import br.com.rodneybarreto.mybatch.domain.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ReaderIT {
+
+    @Autowired
+    private ItemReader<Customer> reader;
+
+    @Test
+    void testFileReader() throws Exception {
+        var flatFileItemReader = (FlatFileItemReader<Customer>) reader;
+        flatFileItemReader.open(new ExecutionContext());
+
+        Customer customer;
+        while((customer = flatFileItemReader.read()) != null) {
+            assertNotNull(customer.getName());
+            assertNotNull(customer.getEmail());
+            assertNotNull(customer.getPixKey());
+        }
+    }
+
+}
---
+++ src/it/java/br/com/rodneybarreto/mybatch/StepConfigIT.java
@@ -0,0 +1,69 @@
+package br.com.rodneybarreto.mybatch;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@SpringBatchTest
+@ActiveProfiles("test")
+class StepConfigIT {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    @Qualifier("appDS")
+    private DataSource dataSource;
+
+    @Autowired
+    private JdbcOperations jdbcTemplate;
+
+    @BeforeEach
+    void before() {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcTemplate.execute("""
+            create table CUSTOMER (
+                id bigint not null auto_increment,
+                name varchar(255) not null,
+                email varchar(255) not null,
+                pix_key varchar(255),
+                pix_key_encrypted varchar(255),
+                primary key(id),
+                unique(email)
+            );
+        """);
+    }
+
+    @Test
+    void testStep() {
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("step");
+        assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+
+        StepExecution stepExecution = jobExecution.getStepExecutions().iterator().next();
+
+        assertEquals(15, stepExecution.getReadCount());
+        assertEquals(15, stepExecution.getWriteCount());
+    }
+
+    @AfterEach
+    void affter() {
+        this.jdbcTemplate.execute("drop table CUSTOMER");
+    }
+
+}
